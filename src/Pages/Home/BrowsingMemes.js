3e93212677089ab0089b64373a733bb0e2f85741
Kamil Dróżdż <108432936+Kamil-Drozdz@users.noreply.github.com>
2023-02-06 08:50:40 +0100

    patch

--- src/Pages/Home/BrowsingMemes.js
+++ src/Pages/Home/BrowsingMemes.js
@@ -101,7 +101,8 @@ function BrowsingMemes({ texts }) {
             </button>
           </div>
         )}
-        {isLoading && <div className='flex'><p>żeby dane załadowały się musisz się zalogować(endpoint niedopracowany)</p><FadeLoader className="mb-4 text-red-600" color="orange" /></div>}
+        {!!data.length && <p className= text-white'>żeby dane załadowały się musisz się zalogować(endpoint niedopracowany)</p>}
+        {isLoading && <FadeLoader className="mb-4 text-red-600" color="orange" />}
         <ToastContainer position="bottom-left" hideProgressBar={false} limit={1} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="dark" />
       </InfiniteScroll>
     </>
