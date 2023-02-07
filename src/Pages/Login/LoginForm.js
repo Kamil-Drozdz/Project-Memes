b3e5c26f14bf8c57c390400313edc42b46d3137c
Kamil Drozdz <kamil12x@wp.pl>
2023-02-07 03:41:15 +0100

    poprawki loginForma

--- src/Pages/Login/LoginForm.js
+++ src/Pages/Login/LoginForm.js
@@ -39,7 +39,7 @@ const LoginForm = ({ texts }) => {
   };
 
   return (
-    <div className="flex h-[92vh] items-center justify-center md:h-[83vh]">
+    <div className="flex h-[93vh] items-center justify-center md:h-[83vh]">
       {showPasswordReset ? (
         <PasswordResetForm setShowPasswordReset={setShowPasswordReset} />
       ) : showRegistration ? (
@@ -55,8 +55,8 @@ const LoginForm = ({ texts }) => {
           </div>
           <br />
           <div className="relative z-0 mb-6 w-full">
-            <input type="password" name="floating_password" id="floating_password" className="peer block w-full appearance-none rounded-none border-0 border-b-2 border-gray-100 bg-transparent py-2.5 px-0 text-sm text-gray-200 focus:border-orange-600 focus:outline-none focus:ring-0" placeholder=" " value={password} onChange={(event) => setPassword(event.target.value)} />
-            <label htmlFor="floating_password" className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-gray-300 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-orange-600">
+            <input type="password" name="floating_password" id="floating_password" className="peer block w-full appearance-none border-0 border-b-2 border-gray-100 bg-transparent py-2.5 px-0 text-sm text-gray-200 focus:border-orange-600 focus:outline-none focus:ring-0" placeholder=" " value={password} onChange={(event) => setPassword(event.target.value)} />
+            <label htmlFor="floating_password" className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform rounded-none text-gray-300 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-orange-600">
               {texts.password}
             </label>
           </div>
